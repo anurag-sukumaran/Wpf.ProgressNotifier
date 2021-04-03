e78e25ca7bc2fb6075f9513b90b401a5795d4649
Anurag Sukumaran <anurag.sukumaran@astechoman.com>
2021-04-03 21:47:25 +0400

    - upload the gif

--- Wpf.ProgressNotifier.Sample/obj/Debug/Wpf.ProgressNotifier.Sample_Content.g.i.cs
+++ Wpf.ProgressNotifier.Sample/obj/Debug/Wpf.ProgressNotifier.Sample_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/progress_notifier_icon.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/progress_notifier.gif")]
 
 
